1d45a8bf416ce9487bd4ef81518851bf3a206757
Ausura Eccleston <asmeccleston@gmail.com>
2026-08-08 18:02:22 +0100

    added a get_playlist_id function

--- video_stats.py
+++ video_stats.py
@@ -10,6 +10,8 @@ API_KEY = os.getenv('API_KEY')
 
 CHANNEL_HANDLE = "MrBeast"
 
+maxResults = 50
+
 def get_playlist_id():
 
     try:
@@ -30,13 +32,55 @@ def get_playlist_id():
 
         channel_playlist_id = channel_items["contentDetails"]["relatedPlaylists"]['uploads']
 
-        print(channel_playlist_id)
+        return channel_playlist_id
 
     except requests.exceptions.RequestException as e:
         raise e
 
+
+def get_video_ids(playlistId):
+
+    video_ids = []
+
+    pageToken = None
+
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=10&playlistId={playlistId}&key={API_KEY}"
+
+    try:
+
+        while True:
+
+            url = base_url
+
+            if pageToken:
+               url += f"&pageToken={pageToken}"
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()     
+
+            for item in data.get('items',[]):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+            pageToken = data.get('nextPageToken')
+
+            if not pageToken:
+                break
+            
+        return video_ids
+            
+    except requests.exceptions.RequestException as e:
+            raise e
+
+#playlist_id = get_playlist_id()
+
+
+
+
 if __name__ == "__main__":
-    print("get_playlist_id will be executed")
-    get_playlist_id()
-else:
-    print("get_playlist_id won't be executed")
+    #print("get_playlist_id will be executed")
+    playlistId = get_playlist_id()
+    print(get_video_ids(playlistId))
